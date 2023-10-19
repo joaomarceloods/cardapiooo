7c312f0122d24738e8c14edafed6a94e7534605f
João Souza <joaomarcelo9393@gmail.com>
2023-10-19 11:03:22 -0300

    Drag and drop columns
    - The columns are draggable
    - The tasks are also draggable and can be dropped in another column
    - I added resetServerContext to avoid client/server markup mismatches

--- src/components/board.tsx
+++ src/components/board.tsx
@@ -6,13 +6,31 @@ import {
   Draggable,
   Droppable,
   OnDragEndResponder,
+  resetServerContext,
 } from "react-beautiful-dnd";
 import initialData from "./initial-data";
 
-function Board() {
+const Board = () => {
+  // react-beautiful-dnd: The resetServerContext function should be used when server side rendering (SSR).
+  // https://github.com/atlassian/react-beautiful-dnd/blob/master/docs/api/reset-server-context.md
+  resetServerContext()
+
   const [state, setState] = useState(initialData);
 
-  const onDragEnd: OnDragEndResponder = (result) => {
+  const moveColumn: OnDragEndResponder = (result) => {
+    setState(state => {
+      const newColumnOrder = Array.from(state.columnOrder);
+      newColumnOrder.splice(result.source.index, 1);
+      newColumnOrder.splice(result.destination!.index, 0, result.draggableId);
+
+      return {
+        ...state,
+        columnOrder: newColumnOrder,
+      };
+    })
+  }
+
+  const moveTask: OnDragEndResponder = (result) => {
     const { destination, source, draggableId } = result;
 
     if (!destination) return;
@@ -58,51 +76,76 @@ function Board() {
         }
       }
     })
+  }
+
+  const onDragEnd: OnDragEndResponder = (result, provided) => {
+    switch (result.type) {
+      case "COLUMN":
+        moveColumn(result, provided)
+        break
+      case "TASK":
+        moveTask(result, provided)
+        break
+    }
   };
 
   return (
     <DragDropContext onDragEnd={onDragEnd}>
       <div>
-        {state.columnOrder.map((columnId) => {
-          const column = state.columns[columnId as keyof typeof state.columns];
-          const tasks = column.taskIds.map(
-            (taskId) => state.tasks[taskId as keyof typeof state.tasks]
-          );
 
-          return (
-            <div key={column.id}>
-              <h3>{column.title}</h3>
+        <Droppable droppableId="board" type="COLUMN">
+          {(provided) => (
+            <div ref={provided.innerRef} {...provided.droppableProps}>
+              {state.columnOrder.map((columnId, index) => {
+                const column = state.columns[columnId as keyof typeof state.columns];
+                const tasks = column.taskIds.map(
+                  (taskId) => state.tasks[taskId as keyof typeof state.tasks]
+                );
 
-              <Droppable droppableId={column.id}>
-                {(provided) => {
+                return (
+                  <Draggable key={column.id} draggableId={column.id} index={index}>
+                    {(provided) => (
+                      <div
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                        ref={provided.innerRef}
+                      >
+                        <h3>{column.title}</h3>
 
-                  return (
-                    <div ref={provided.innerRef} {...provided.droppableProps}>
-                      {tasks.map((task, index) => {
-                        const { id, content } = task;
+                        <Droppable droppableId={column.id} type="TASK">
+                          {(provided) => {
+                            return (
+                              <div ref={provided.innerRef} {...provided.droppableProps} style={{border: "1px solid black", margin: 8, minHeight: 20 }}>
+                                {tasks.map((task, index) => {
 
-                        return (
-                          <Draggable key={id} draggableId={id} index={index}>
-                            {(provided) => (
-                              <div
-                                {...provided.draggableProps}
-                                {...provided.dragHandleProps}
-                                ref={provided.innerRef}
-                              >
-                                {content}
+                                  return (
+                                    <Draggable key={task.id} draggableId={task.id} index={index}>
+                                      {(provided) => (
+                                        <div
+                                          {...provided.draggableProps}
+                                          {...provided.dragHandleProps}
+                                          ref={provided.innerRef}
+                                        >
+                                          {task.content}
+                                        </div>
+                                      )}
+                                    </Draggable>
+                                  );
+                                })}
+                                {provided.placeholder}
                               </div>
-                            )}
-                          </Draggable>
-                        );
-                      })}
-                      {provided.placeholder}
-                    </div>
-                  );
-                }}
-              </Droppable>
+                            );
+                          }}
+                        </Droppable>
+                      </div>
+                    )}
+                  </Draggable>
+                )
+              })}
+              {provided.placeholder}
             </div>
-          );
-        })}
+          )}
+        </Droppable>
       </div>
     </DragDropContext>
   );
