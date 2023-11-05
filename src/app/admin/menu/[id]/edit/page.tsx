12420b65c8b5e14ee9052d3ec8344fb1ca7dfb42
João Souza <joaomarcelo9393@gmail.com>
2023-11-05 18:28:35 -0300

    Fix database connection on menu edit page

--- src/app/admin/menu/[id]/edit/page.tsx
+++ src/app/admin/menu/[id]/edit/page.tsx
@@ -2,8 +2,11 @@ import { DBMenu } from '@/mongoose/model'
 import { FC } from 'react'
 import { normalizeState } from './provider/normalizr'
 import Provider from './provider/provider'
+import { connectDatabase } from '@/mongoose/connect'
 
 const Page: FC<{ params: { id: string } }> = async ({ params: { id } }) => {
+  await connectDatabase()
+
   const initialState = await DBMenu.findById(id)
     .exec()
     .then((d) => d.toJSON())
