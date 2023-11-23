d33252731d2bcce7f10ba8dd930190233e232bb0
João Souza <joaomarcelo9393@gmail.com>
2023-11-23 15:38:38 -0300

    Replace file input by antd Upload component
    https://ant.design/components/upload

--- src/app/admin/menu/[id]/edit/components/items/product-photo.tsx
+++ src/app/admin/menu/[id]/edit/components/items/product-photo.tsx
@@ -1,56 +1,79 @@
+import { LoadingOutlined, PlusOutlined } from '@ant-design/icons'
 import { PutBlobResult } from '@vercel/blob'
+import { Upload, UploadProps, message } from 'antd'
+import type { UploadChangeParam } from 'antd/es/upload'
+import type { RcFile, UploadFile } from 'antd/es/upload/interface'
 import Image from 'next/image'
 import { FC, useState } from 'react'
 import { useReducerDispatch, useReducerState } from '../../provider/provider'
 import { Entity, Reducer } from '../../provider/types'
+
+const beforeUpload = (file: RcFile) => {
+  const isValidFormat = /image\/jpeg|png|webp/.test(file.type)
+  if (!isValidFormat) {
+    message.error('You can only upload JPG, PNG or WEBP images')
+  }
+  const isValidSize = file.size / 1024 / 1024 < 4.5
+  if (!isValidSize) {
+    message.error('Image must be smaller than 4.5MB')
+  }
+  return isValidFormat && isValidSize
+}
 
 const ProductPhoto: FC<{ menuId: string; itemId: string }> = ({
   menuId,
   itemId,
 }) => {
   const state = useReducerState()
-  const dispatch = useReducerDispatch()
   const item = state.entities.items[itemId]
-  const { title, price, description, photo } = item.data as Entity.ProductData
+  const { photo } = item.data as Entity.ProductData
 
-  const [uploading, setUploading] = useState<boolean>(false)
+  const dispatch = useReducerDispatch()
+  const [loading, setLoading] = useState(false)
 
-  const handleChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
-    const file = e.target.files?.[0]
-    if (!file) return
-    setUploading(true)
-
-    // TODO: handle error
-    const response = await fetch(
-      `/api/product-photo?menu=${menuId}&item=${itemId}`,
-      {
-        method: 'POST',
-        body: file,
-      }
-    )
-
-    setUploading(false)
-    const newBlob: PutBlobResult = await response.json()
-
-    dispatch({
-      type: Reducer.ActionType.ChangeItem,
-      payload: { value: newBlob.url, id: itemId, property: 'photo' },
-    })
+  const handleChange: UploadProps['onChange'] = (
+    info: UploadChangeParam<UploadFile>
+  ) => {
+    if (info.file.status === 'uploading') {
+      setLoading(true)
+    } else if (info.file.status === 'done') {
+      setLoading(false)
+      const blob: PutBlobResult = info.file.response
+      dispatch({
+        type: Reducer.ActionType.ChangeItem,
+        payload: { value: blob.url, id: itemId, property: 'photo' },
+      })
+    }
   }
 
+  const uploadButton = (
+    <div>
+      {loading ? <LoadingOutlined /> : <PlusOutlined />}
+      <div style={{ marginTop: 8 }}>Photo</div>
+    </div>
+  )
+
+  // TODO: move menuId and itemId into `data` prop
   return (
-    <>
-      <input type="file" onChange={handleChange} />
-      {uploading ? (
-        <span>Uploading...</span>
+    <Upload
+      name="photo"
+      listType="picture-card"
+      className="avatar-uploader"
+      showUploadList={false}
+      action={`/api/product-photo?menu=${menuId}&item=${itemId}`}
+      beforeUpload={beforeUpload}
+      onChange={handleChange}
+    >
+      {loading ? (
+        uploadButton
       ) : photo ? (
-        <a href={photo}>
-          <Image src={photo} alt="Product photo" width={30} height={30} />
-        </a>
+        <div style={{ position: 'relative', width: 84, height: 84 }}>
+          <Image src={photo} alt="Product photo" fill objectFit="cover" />
+        </div>
       ) : (
-        <span>No photo</span>
+        uploadButton
       )}
-    </>
+    </Upload>
   )
 }
 
--- src/app/api/product-photo/route.ts
+++ src/app/api/product-photo/route.ts
@@ -1,12 +1,22 @@
-import { put } from '@vercel/blob'
+import { del, list, put } from '@vercel/blob'
 import { NextResponse } from 'next/server'
 
 export async function POST(request: Request): Promise<NextResponse> {
+  const formData = await request.formData()
+  const photo = formData.get('photo') as File | null
+
+  if (!photo) {
+    return NextResponse.json({ error: 'Photo Required' }, { status: 400 })
+  }
+
   const { searchParams } = new URL(request.url)
   const menu = searchParams.get('menu')
   const item = searchParams.get('item')
-  const pathname = `product-photo/menu-${menu}/item-${item}`
-  // TODO: handle null body
-  const blob = await put(pathname, request.body!, { access: 'public' })
+  const pathname = `menu/${menu}/item/${item}/product-photo/${photo.name}`
+  const blob = await put(pathname, photo, { access: 'public' })
+
+  const oldPhotos = await list({ prefix: pathname })
+  oldPhotos.blobs.forEach(async (blob) => await del(blob.url))
+
   return NextResponse.json(blob)
 }
