79589cf92e26a27a9d7ef44c4f9326a10e97fd95
João Souza <joaomarcelo9393@gmail.com>
2023-11-27 21:38:45 -0300

    On save menu, revalidate page cache
    - It prevents showing outdated content when next visiting the page

--- src/app/api/menu/route.ts
+++ src/app/api/menu/route.ts
@@ -1,9 +1,11 @@
 import { Menu } from '@/database/model'
+import { revalidatePath } from 'next/cache'
 
 export async function POST(request: Request) {
   const json = await request.json()
   const menu = await Menu.findById(json.id).exec()
   menu.set(json)
   await menu.save()
+  revalidatePath(`/admin/menu/${json.id}`)
   return Response.json(null)
 }
