0d91c61f8da882aa0281e86854ff020a202edf66
João Souza <joaomarcelo9393@gmail.com>
2023-11-30 14:19:49 -0300

    Don't authorize menu if its business not found

--- src/authorize.ts
+++ src/authorize.ts
@@ -9,22 +9,16 @@ class NotAuthorizedError extends Error {
 
 export const authorizeBusiness = async (businessId: string) => {
   const { userId } = auth()
-
-  if (!userId) {
-    throw new NotAuthorizedError()
-  }
-
+  if (!userId) throw new NotAuthorizedError()
   const businessUser = await BusinessUser.exists({
     user: userId,
     business: businessId,
   })
-
-  if (!businessUser) {
-    throw new NotAuthorizedError()
-  }
+  if (!businessUser) throw new NotAuthorizedError()
 }
 
 export const authorizeMenu = async (menuId: string) => {
   const business = await Business.findOne({ menus: menuId }).exec()
+  if (!business) throw new NotAuthorizedError()
   await authorizeBusiness(business.id)
 }
